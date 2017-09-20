1e6d85a3fdc1f5764a33dec4fab3e217faa8d06f
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2017-09-20 12:18:35 +1200

    Applying machine learning to individual users

--- sql.py
+++ sql.py
@@ -10,34 +10,58 @@ y_train = []
 X_test = []
 y_test = []
 readingID = []
+currentUser = 0;
 
-def setPosture(predictions):
-    global readingID
-    # print(readingID , predictions)
-    for i in range(len(predictions)):
-    
-        connection = pymysql.connect(host='localhost', user='root', password='12345678', db='PostureAlert')
-        cursor=connection.cursor()
-        print("UPDATE SensorReadings SET Posture = %s WHERE ReadingID = %s", str(predictions[i]), str(readingID[i]))
-        sql=("UPDATE SensorReadings SET Posture = %s WHERE ReadingID = %s;")
-        cursor.execute(sql, (str(predictions[i]), str(readingID[i])))
-        connection.commit()
-        print('Changed', cursor.rowcount)
-        cursor.close()
-        connection.close()
+
+def getNewReadings():
+    global features, labels, X_test, y_test, readingID, currentUser
+    features = []
+    labels = []
+    readingID = []
+    # new data set
+    connection = pymysql.connect(host='13.55.201.70', user='root', password='12345678', db='PostureAlert')
+    cursor=connection.cursor()
+    sql=("SELECT UserID FROM SensorReadings WHERE Posture IS NULL LIMIT 1;")
+    print(sql)
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    # print("Printing data........")
+    # print(data)
+    currentUser = data[0][0]
+    print("Printing currentUser: " + str(currentUser))
+
+    sql=("SELECT * FROM SensorReadings WHERE Posture IS NULL AND UserID = " + str(currentUser) + ";")
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    # print(len(data))
+
+    cursor.close()
+    connection.close()
+
+    for row in range(len(data)):
+        features.append(data[row][1:9])
+        readingID.append(data[row][0])
+
+    X_test = features
+    y_test = labels
+    # print(X_test)
+    # print(y_test)  
+    print('Test sample size = ', len(X_test), len(y_test))
+
 
 # ---------------------------------------------------------------------------------------------------------------------------------
 def getTrainDataset():
-    global features, labels, X_train, y_train
+    global features, labels, X_train, y_train, currentUser
     features = []
     labels = []
     # trainning set
-    connection = pymysql.connect(host='localhost', user='root', password='12345678', db='PostureAlert')
+    connection = pymysql.connect(host='13.55.201.70', user='root', password='12345678', db='PostureAlert')
     cursor=connection.cursor()
-    sql=("SELECT * FROM SensorReadings WHERE Posture IS NOT NULL")
+    sql=("SELECT * FROM SensorReadings WHERE Posture IS NOT NULL AND UserID = " + str(currentUser) + ";")
+    print(sql)
     cursor.execute(sql)
     data=cursor.fetchall()
-    # print(data)
+    print(data)
     cursor.close()
     connection.close()
 
@@ -51,32 +75,22 @@ def getTrainDataset():
     # print(y_train)
     print('Train sample size = ', len(X_train), len(y_train))
 
-
 # ------------------------------------------------------------------------------------------------------------------------------
-def getNewReadings():
-    global features, labels, X_test, y_test, readingID
-    features = []
-    labels = []
-    readingID = []
-    # new data set
-    connection = pymysql.connect(host='localhost', user='root', password='12345678', db='PostureAlert')
-    cursor=connection.cursor()
-    sql=("SELECT * FROM SensorReadings WHERE Posture IS NULL")
-    cursor.execute(sql)
-    data=cursor.fetchall()
-    # print(data)
-    cursor.close()
-    connection.close()
 
-    for row in range(len(data)):
-        features.append(data[row][1:9])
-        readingID.append(data[row][0])
-
-    X_test = features
-    y_test = labels
-    # print(X_test)
-    # print(y_test)  
-    print('Test sample size = ', len(X_test), len(y_test))
+def setPosture(predictions):
+    global readingID
+    # print(readingID , predictions)
+    for i in range(len(predictions)):
+    
+        connection = pymysql.connect(host='13.55.201.70', user='root', password='12345678', db='PostureAlert')
+        cursor=connection.cursor()
+        print("UPDATE SensorReadings SET Posture = %s WHERE ReadingID = %s", str(predictions[i]), str(readingID[i]))
+        sql=("UPDATE SensorReadings SET Posture = %s WHERE ReadingID = %s;")
+        cursor.execute(sql, (str(predictions[i]), str(readingID[i])))
+        connection.commit()
+        print('Changed', cursor.rowcount)
+        cursor.close()
+        connection.close()
 
 # ------------------------------------------------------------------------------------------------------------------------------
 
@@ -151,19 +165,19 @@ import time
 from sklearn import metrics, tree, neighbors
 algos = {
             # 'Tree': tree.DecisionTreeClassifier(),
-            # 'SKLearn K-Nearest Neighbor': neighbors.KNeighborsClassifier(),
+             'SKLearn K-Nearest Neighbor': neighbors.KNeighborsClassifier(),
             # 'Random': RandomClassifier(),
             # 'Nearest Neighbor': NNClassifier(),
-            '3-Nearest Neighbor': KNNClassifier(3),
+            #'3-Nearest Neighbor': KNNClassifier(3),
             # '7-Nearest Neighbor': KNNClassifier(7),
             # '15-Nearest Neighbor': KNNClassifier(15),
         }
 
 while (1):
     for outputLabel, algo in algos.items():
-        getTrainDataset()
         getNewReadings()
-        print("Testing ", outputLabel)
+        getTrainDataset()
+        # print("Testing ", outputLabel)
         predictions = measure(algo, outputLabel)
         setPosture(predictions)
     time.sleep(10)
